3d8df8024dd9b7f46f30f60beaf7168b9552e923
Loïc DS <desousa.loic8@gmail.com>
2024-05-22 09:21:58 +0200

    formation add

--- controllers/admin/formation.controller.php
+++ controllers/admin/formation.controller.php
@@ -1,10 +1,19 @@
 <?php
 
-function index()
+function viewFormation()
 {
-    require './models/admin/formation.manager.php';
+    require '../models/admin/formation.manager.php';
     $formations = getAllFormation();
 
     $template = "../views/admin/formation.html.php";
+    require "../views/layout.html.php";
+}
+
+function addFormation()
+{
+    require '../models/admin/formation.manager.php';
+    $formation = createFormation();
+
+    $template = "../views/admin/formation/addformation.html.php";
     require "../views/layout.html.php";
 }
