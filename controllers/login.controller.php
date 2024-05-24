0958cf17eed311647d7361c075e088a5f59a6033
Nathan Weber <81427220+Spiritus-Fripus@users.noreply.github.com>
2024-05-24 11:44:08 +0200

    changement au niveau du controller login

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -4,12 +4,32 @@ function formLoginAction()
 {
     $title = "LOGIN MNS";
     require '../views/home/login/index-login.html.php';
+    $type_user = '';
+    require '../models/login/login.manager.php';
 
     if (isset($_POST['send'])) {
 
         if (!empty($_POST['email']) && !empty($_POST['password'])) {
-            require '../models/login/login.manager.php';
-            connect($_POST['email'], $_POST['password']);
+            $response = connect($_POST['email']);
+            if ($response) {
+                if (password_verify($_POST['password'], $response['user_password'])) {
+                    switch ($response['user_type_id']) {
+                        case 1:
+                            $type_user = 'admin';
+                            break;
+                        case 2:
+                            $type_user = 'teacher';
+                            break;
+                        case 3:
+                            $type_user = 'student';
+                            break;
+                    }
+                    $_SESSION['user_type'] = $type_user;
+                    $_SESSION['user_mail'] = $_POST['email'];
+                    header("Location: ?controller=$type_user&action=index");
+                    exit();
+                }
+            }
         } else {
             echo "Veuillez remplir tous les champs.";
         }
