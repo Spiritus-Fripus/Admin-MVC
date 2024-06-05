a0a686c7ea192fda2faa4410e1014f08abcfbe8b
Nathan Weber <81427220+Spiritus-Fripus@users.noreply.github.com>
2024-06-05 16:23:20 +0200

    changement dans les liens de sidebar

--- views/sidebar/sidebar.html.php
+++ views/sidebar/sidebar.html.php
@@ -2,18 +2,21 @@
     <ul>
         <li><a href='?controller=<?= $_SESSION['user_type'] ?>&action=index'>Accueil</a></li>
         <li><a href='?controller=<?= $_SESSION['user_type'] ?>&action=profil'>Profil</a></li>
+
         <?php if ($_SESSION['user_type'] === 'admin') { ?>
             <li><a href="?controller=formation&action=viewFormation">Formation</a></li>
             <li><a href="?controller=Promotion&action=viewPromotion">Promotion</a></li>
+            <li><a href="?controller=delay&action=viewDelay">Retards</a></li>
+            <li><a href="?controller=absence&action=viewOwnAbsence">Absences</a></li>
         <?php } ?>
+
         <?php if ($_SESSION['user_type'] === 'teacher') { ?>
-            <li><a href="?controller=formation&action=viewFormation">Formation</a></li>
             <li><a href="?controller=student&action=viewClasses">Classes</a></li>
         <?php } ?>
+
         <?php if ($_SESSION['user_type'] === 'student') { ?>
             <li><a href="?controller=delay&action=viewDelay">Retards</a></li>
             <li><a href="?controller=absence&action=viewOwnAbsence">Absences</a></li>
         <?php } ?>
-        <li><a href="?controller=login&action=logout">Déconnexion</a></li>
     </ul>
 </div>
