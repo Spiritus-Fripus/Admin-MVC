83621d68c0d84f466d98bb1489fc0a98661f6b6a
keurbi <148528533+keurbi@users.noreply.github.com>
2024-06-06 14:24:52 +0200

    candi

--- controllers/candidate.controller.php
+++ controllers/candidate.controller.php
@@ -4,4 +4,14 @@ require '../config/config.php';
 
 function viewCandidate()
 {
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        addCandidate();
+        //redirection vers page candidatures
+        header("Location: ?controller=candidate&action=viewcandidate");
+    }
+
+    $cssFile = '/css/candidate-style.css';
+    $template = '../views/candidate.html.php';
+    require "../views/layouts/layout.html.php";
 }
